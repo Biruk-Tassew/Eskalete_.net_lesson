7100768a8d6e0fd1400715c3fbd631398c1423c5
Biruk Tassew <biruktassew21@gmail.com>
2023-04-26 10:54:40 +0300

    fix(backend: basic setup for infrastructure and persistance

---
+++ src/infrastructure/HR.LeaveManagement.Persistance/LeaveManagementDbContext.cs
@@ -0,0 +1,36 @@
+﻿using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using HR.LeaveManagement.Domain.Common;
+
+namespace HR.LeaveManagement.Persistance;
+public class LeaveManagementDbContext : DbContext
+{
+    public LeaveManagementDbContext(DbContextOptions<LeaveManagementDbContext> options) : base(options)
+    {
+    
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagementDbContext).Assembly);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+        {
+            entry.Entity.LastModifiedDate = DateTime.Now;
+
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.DateCreated = DateTime.Now;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+    
+    public DbSet<LeaveType> LeaveTypes;
+    public DbSet<LeaveRequest> LeaveRequests;
+    public DbSet<LeaveAllocation> LeaveAllocations;
+}
